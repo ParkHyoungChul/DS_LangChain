5580b09c8aa3569ed7a807aa783fe4f4da9cd68a
ParkHyoungChul <138740553+ParkHyoungChul@users.noreply.github.com>
2025-10-22 12:17:59 +0900

    Update streamlit_rag_memory_cloud.py

--- streamlit_rag_memory_cloud.py
+++ streamlit_rag_memory_cloud.py
@@ -7,7 +7,7 @@ from operator import itemgetter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 # from langchain.chains.combine_documents import create_stuff_documents_chain
 # from langchain.chains import create_history_aware_retriever, create_retrieval_chain
@@ -20,7 +20,7 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from langchain_community.vectorstores import Chroma
+
 os.environ["OPENAI_API_KEY"] = st.secrets['OPENAI_API_KEY']
 
 #cache_resource로 한번 실행한 결과 캐싱해두기
@@ -160,5 +160,6 @@ if prompt_message := st.chat_input("Your question"):
             with st.expander("참고 문서 확인"):
                 for doc in response['context']:
                     st.markdown(doc.metadata['source'], help=doc.page_content)
+
 
 
